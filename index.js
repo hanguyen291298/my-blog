4ff7afa8d6bd9c705880f68948005698f34009c0
hanguyen291298 <thuyha291298@gmail.com>
2023-12-22 20:12:03 +0700

    edit function of loading text

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//CREATE AFFECTION TO MENU DROPDOWN
 const DROPDOWN = document.querySelector(".dropdown")
 const CLOSE_BUTTON = document.querySelector(".close-button")
 const TOUCH_BUTTON = document.querySelector(".touch-button")
@@ -13,3 +14,23 @@ TOUCH_BUTTON.addEventListener("click", () => {
     HEAD_CONTAINER.setAttribute("style", "visibility: hidden")
 })
 
+// CREATE EFFECTION TO THE ANIMATION HEADER TEXT
+const text = document.querySelector(".dynamic-text")
+
+const textload = function(){
+    setTimeout(()=>{
+        text.textContent = " self-taught."
+    }, 4005)
+    setTimeout(()=>{
+        text.textContent = "n inspier."
+    }, 8010)
+    setTimeout(()=>{
+        text.textContent = " developer."
+    }, 12015);
+    setTimeout(()=>{
+        text.textContent = " book lover."
+    }, 16020)
+}
+
+textload()
+setInterval(textload, 16020)
