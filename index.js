dd1b091475d01a4cc3f6fbacd522995189c9684c
hanguyen291298 <thuyha291298@gmail.com>
2023-12-22 15:31:50 +0700

    edit the advenlistener when dropdown is click

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const DROPDOWN = document.querySelector(".dropdown")
 const CLOSE_BUTTON = document.querySelector(".close-button")
 const TOUCH_BUTTON = document.querySelector(".touch-button")
+const HEAD_CONTAINER = document.querySelector(".head-container")
 
 CLOSE_BUTTON.addEventListener("click", () => {
     DROPDOWN.setAttribute("style", "visibility: hidden")
+    HEAD_CONTAINER.setAttribute("style", "visibility: visible")
 })
 
 TOUCH_BUTTON.addEventListener("click", () => {
-    DROPDOWN.setAttribute("style", "visibility: visible")
+    DROPDOWN.setAttribute("style", "visibility: visible");
+    HEAD_CONTAINER.setAttribute("style", "visibility: hidden")
 })
 
