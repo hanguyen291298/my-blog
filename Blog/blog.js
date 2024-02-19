8dac0b2e127ff7627abffc8a8855f520d930bc48
hanguyen291298 <thuyha291298@gmail.com>
2024-02-19 18:14:29 +0700

    add addventlistenter to change the background

--- Blog/blog.js
+++ Blog/blog.js
@@ -1,7 +1,10 @@
-
-const themesContainer = document.querySelector(".themes-container")
+const toggleButton = document.querySelector(".toggle-button")
 document.querySelector(".toggle-button").addEventListener("click", ()=>{
-    document.body.classList.toggle("dark")
-    const currentRotation = parseInt(getComputedStyle(themesContainer).getPropertyValue('--rotation'))
-    themesContainer.style.setProperty('--rotate', currentRotation + 180)
+    document.body.classList.toggle("light")
+    if (document.body.classList.contains("light")){
+        toggleButton.innerHTML = "Light On"
+    }
+    else{
+        toggleButton.innerHTML = "Light Off"
+    }
 })
