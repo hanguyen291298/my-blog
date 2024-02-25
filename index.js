e99e1964b3c88a582a822ef6ac0d723726ce0fe8
hanguyen291298 <thuyha291298@gmail.com>
2024-02-25 20:35:20 +0700

    change entire main page information and class name

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //CREATE AFFECTION TO MENU DROPDOWN
 const DROPDOWN = document.querySelector(".dropdown")
 const CLOSE_BUTTON = document.querySelector(".close-button")
-const TOUCH_BUTTON = document.querySelector(".touch-button")
-const PHONE_TOUCH_BUTTON = document.querySelector(".phone-menu")
-const HEAD_CONTAINER = document.querySelector(".head-container")
+const TOUCH_BUTTON = document.querySelector(".nav-btn")
+const PHONE_TOUCH_BUTTON = document.querySelector(".phone-nav")
+const HEADER_CONTAINER = document.querySelector(".header-container")
 
 CLOSE_BUTTON.addEventListener("click", () => {
     DROPDOWN.setAttribute("style", "visibility: hidden")
-    HEAD_CONTAINER.setAttribute("style", "visibility: visible")
+    HEADER_CONTAINER.setAttribute("style", "visibility: visible")
 })
 
 TOUCH_BUTTON.addEventListener("click", () => {
     DROPDOWN.setAttribute("style", "visibility: visible");
-    HEAD_CONTAINER.setAttribute("style", "visibility: hidden")
+    HEADER_CONTAINER.setAttribute("style", "visibility: hidden")
 })
 PHONE_TOUCH_BUTTON.addEventListener("click", () => {
     DROPDOWN.setAttribute("style", "visibility: visible");
-    HEAD_CONTAINER.setAttribute("style", "visibility: hidden")
+    HEADER_CONTAINER.setAttribute("style", "visibility: hidden")
 })
 
 // CREATE EFFECTION TO THE ANIMATION HEADER TEXT
@@ -24,18 +24,20 @@ const text = document.querySelector(".dynamic-text")
 
 const textload = function(){
     setTimeout(()=>{
-        text.textContent = " self-taught."
-    }, 4002)
+        text.textContent = " a self-taught."
+    }, 4000)
     setTimeout(()=>{
-        text.textContent = "n inspier."
-    }, 8004)
+        text.textContent = " an inspier."
+    }, 8000)
     setTimeout(()=>{
-        text.textContent = " developer."
-    }, 12006);
+        text.textContent = " a developer."
+    }, 12000);
     setTimeout(()=>{
-        text.textContent = " book lover."
-    }, 16006)
+        text.textContent = " a book lover."
+    }, 16000)
 }
 
 textload()
-setInterval(textload, 16006)
+setInterval(textload, 16000)
+
+
